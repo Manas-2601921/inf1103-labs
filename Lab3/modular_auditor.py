9a2cb8cb8ac8480bc46a6f7c39e233f751227c61
Manas <2601921@sit.singaporetech.edu.sg>
2026-09-20 16:25:26 +0800

    added process_delivery(), calculate_tax(), generate_report()  functions

--- Lab3/modular_auditor.py
+++ Lab3/modular_auditor.py
@@ -21,3 +21,16 @@ def get_valid_input():
             return "quit"
         else:
             return intInputToValidate
+        
+def process_delivery(current_total, new_value):
+    new_total = current_total + new_value
+    return new_total
+
+def calculate_tax(value):
+    taxed_amt = 0.10*value
+    final_taxed_amt = taxed_amt + value
+    return final_taxed_amt
+
+def generate_report(final_total_value, userFailedAttempts):
+    print("The total units in your inventory are: ", final_total_value)
+    print("Number of failed/rejected entries: ",userFailedAttempts)
